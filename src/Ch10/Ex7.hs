e13842454fce7062864c6ad8c3e00abb3994e87f
Heneli Kailahi <heneli@tvisioninsights.com>
2020-05-29 15:11:50 -0400

    Minor fixes

--- src/Ch10/Ex7.hs
+++ src/Ch10/Ex7.hs
@@ -8,8 +8,10 @@ import Ch5.Classes.Heap
 Exercise 10.7
 Inlining the LazyBinomialHeap functor of Section 6.4.1 as described above yields the types
 
+```
 datatype Tree = Node of int x Heap x Tree list
 datatype Heap = E | NE of Elem.T x Tree list susp
+```
 
 Complete this implementation of bootstrapped heaps
 -}
@@ -27,7 +29,7 @@ rank (Node r _ _) = r
 
 root :: Tree a -> a
 root (Node _ (NE x _) _) = x
-root _ = error "dasfas"
+root _                   = error "dasfas"
 
 tree :: a -> Tree a
 tree x = Node 0 (NE x []) []
@@ -52,39 +54,42 @@ mrg ts1@(t1 : ts1') ts2@(t2 : ts2')
   | otherwise = insTree (link t1 t2) (mrg ts1' ts2')
 
 removeMinTree :: Ord a => [Tree a] -> (Tree a, [Tree a])
-removeMinTree [] = error "empty heap"
+removeMinTree []  = error "empty heap"
 removeMinTree [t] = (t, [])
 removeMinTree (t : ts)
   | root t < root t' = (t, ts)
   | otherwise = (t', t : ts')
   where (t', ts') = removeMinTree ts
 
+unit :: a -> BootstrapBinomialHeap a
+unit x = NE x []
+
 instance (Ord a) => Heap BootstrapBinomialHeap a where
   empty :: BootstrapBinomialHeap a
   empty = E
 
-  isEmpty :: BootstrapBinomialHeap a
+  isEmpty :: BootstrapBinomialHeap a -> Bool
   isEmpty E = True
   isEmpty _ = False
 
   insert :: a -> BootstrapBinomialHeap a -> BootstrapBinomialHeap a
-  insert x E = NE x []
+  insert x E = unit x
   insert x (NE y ts)
-    | x <= y    = NE x (insTree (tree y) ts)
-    | otherwise = NE y (insTree (tree x) ts)
+    | x <= y    = NE x $ insTree (tree y) ts
+    | otherwise = NE y $ insTree (tree x) ts
 
-  merge :: a -> BootstrapBinomialHeap a -> BootstrapBinomialHeap a
+  merge :: BootstrapBinomialHeap a -> BootstrapBinomialHeap a -> BootstrapBinomialHeap a
   merge E h = h
   merge h E = h
   merge (NE x ts1) (NE y ts2)
-    | x <= y    = NE x (insTree (tree y) (mrg ts1 ts2))
-    | otherwise = NE y (insTree (tree x) (mrg ts1 ts2))
+    | x <= y    = NE x . insTree (tree y) $ mrg ts1 ts2
+    | otherwise = NE y . insTree (tree x) $ mrg ts1 ts2
 
-  findMin :: BootstrapBinomialHeap a -> a
+  findMin :: BootstrapBinomialHeap a -> Either HeapEmpty a
   findMin E = Left HeapEmpty
   findMin (NE x _) = Right x
 
-  deleteMin :: BootstrapBinomialHeap a -> BootstrapBinomialHeap a
+  deleteMin :: BootstrapBinomialHeap a -> Either HeapEmpty (BootstrapBinomialHeap a)
   deleteMin E = Left HeapEmpty
-  deleteMin (NE _ ts) = Right $ NE x (mrg (reverse ts1) ts2)
+  deleteMin (NE _ ts) = Right . NE x $ mrg (reverse ts1) ts2
     where (Node _ (NE x []) ts1, ts2) = removeMinTree ts
